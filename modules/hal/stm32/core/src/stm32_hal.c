8ea56314ebf98fb0bbe7d5f9e1410da123e11259
bmdk <bmdk@users.noreply.github.com>
2021-01-17 12:42:55 +0100

    add devid for F411

--- modules/hal/stm32/core/src/stm32_hal.c
+++ modules/hal/stm32/core/src/stm32_hal.c
@@ -95,6 +95,9 @@ int cmd_devid(int argc, char *argv[])
   case 0x419:
     idstr = "F42x/43x";
     break;
+  case 0x431:
+    idstr = "F411";
+    break;
   case 0x435:
     idstr = "L43x/44x";
     break;
