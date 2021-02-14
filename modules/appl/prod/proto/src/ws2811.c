65d64c4a81d31b1113ca131bd3b235b331073190
bmdk <bmdk@users.noreply.github.com>
2021-02-14 02:35:06 +0100

    ws2811 fb/reorganize

--- modules/appl/prod/proto/src/ws2811.c
+++ modules/appl/prod/proto/src/ws2811.c
@@ -28,8 +28,16 @@
 #include "stm32_hal_board.h"
 #include "stm32_hal_gpio.h"
 #include "stm32_timer.h"
+#include "fb.h"
+#include "io.h"
 
 #if STM32_F411BP || STM32_F401BP
+#define WS2811_USE_DMA 1
+#else
+#define WS2811_USE_BDMA 1
+#endif
+
+#if WS2811_USE_DMA
 #include "stm32_hal_dma.h"
 #else
 #include "stm32_hal_bdma.h"
@@ -44,7 +52,7 @@
 #define WCPCCLOCKS(period_ns) \
   (WSCLOCKS * (period_ns) + WSPERIOD_NS / 2) / WSPERIOD_NS
 
-#if STM32_F411BP || STM32_F401BP
+#if WS2811_USE_DMA
 #define WSBIT 0
 #define WSIRQ 57
 #define WSGPIO 1
@@ -63,7 +71,7 @@ static const unsigned int compare[2] = { WCPCCLOCKS(350), WCPCCLOCKS(700) };
 #define PIXELS 256
 static unsigned char buf[24 * PIXELS];
 
-#if STM32_F411BP || STM32_F401BP
+#if WS2811_USE_DMA
 static void ws2811_tx()
 {
   FAST_LOG('W', "ws2811 tx start\n", 0, 0);
@@ -135,10 +143,21 @@ void enc_col(unsigned int pix, unsigned int val, unsigned int string)
 
   for (i = 0; i < 24; i += 4) {
     unsigned int _val = (val >> (20 - i)) & 0xf;
-    void *a = &buf[idx + i];
     unsigned int v;
+    void *a;
 
     v = ((_val >> 3) + (_val << 6) + (_val << 15) + (_val << 24)) & MASK;
+
+#if WS2811_STRING_GRB
+    if (i < 8)
+      a = &buf[idx + i + 8];
+    else if (i < 16)
+      a = &buf[idx + i - 8];
+    else
+      a = &buf[idx + i];
+#else
+    a = &buf[idx + i];
+#endif
 
     wrint(a, v, string);
   }
@@ -179,7 +198,7 @@ unsigned int scale(unsigned int v, unsigned int s)
 
 void irq_ws2811(void *data)
 {
-#if STM32_F411BP || STM32_F401BP
+#if WS2811_USE_DMA
   stm32_dma_irq_ack(1, 1, DMA_IER_TCIF);
 #else
   stm32_bdma_irq_ack(BDMA1_BASE, 1, IER_TCIF);
@@ -187,48 +206,179 @@ void irq_ws2811(void *data)
   FAST_LOG('W', "irq_ws2811\n", 0, 0);
 }
 
+#if WS2811_DISPLAY_64X8_CLOCK
+#include <stdio.h>
+
+#include "hal_rtc.h"
+
+extern const char font1[];
+
+unsigned int colours[] =
+{ 0x010000, 0x010100, 0x000100, 0x000101, 0x000001, 0x010101, 0x010000,
+  0x010100 };
+
+void disp_char(fb_t *fb, int x, int y, char c)
+{
+  const char *b = &font1[2 + c * 8];
+  int i;
+
+  for (i = 0; i < 8; i++) {
+    unsigned ch = *(b + i);
+    unsigned int j;
+
+    for (j = 0; j < 8; j++) {
+      unsigned int v = (ch >> (8 - 1 - j)) & 1;
+
+      if (v)
+        fb_draw(fb, x + j, y + i, colours[i]);
+    }
+  }
+}
+
+void disp_str(fb_t *fb, int x, int y, char *s, unsigned int slen)
+{
+  unsigned int k;
+
+  for (k = 0; k < slen; k++)
+    disp_char(fb, k * 8 + x, y, s[k]);
+}
+
+void fb_to_ws2811disp(fb_t *fb)
+{
+  unsigned int x, y, h, w;
+  unsigned int *data;
+
+  w = fb_width(fb);
+  h = fb_height(fb);
+  data = fb_get(fb);
+
+  for (x = 0; x < w; x++) {
+    for (y = 0; y < h; y++) {
+      if (x % 2 == 0)
+        enc_col(x * h + y, data[x * h + y], WSBIT);
+      else
+        enc_col(x * h + (h - 1 - y), data[x * h + y], WSBIT);
+    }
+  }
+
+  ws2811_tx();
+}
+
+/* *INDENT-OFF* */
+static const char *dayname[] = {
+ "THINGY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY",
+ "FRIDAY", "SATURDAY", "SUNDAY"
+};
+/* *INDENT-ON* */
+
+static const char *get_dayname(unsigned int daynum)
+{
+  if (daynum > ARRSIZ(dayname))
+    daynum = 0;
+
+  return dayname[daynum];
+}
+
+void task_led(void *arg)
+{
+  unsigned int w = 32, h = 8;
+  char message[64];
+
+  irq_register("ws2811", irq_ws2811, 0, WSIRQ);
+
+  fb_t *fb = fb_init(w, h, 24);
+
+  for (;;) {
+    int i, len, ofs;
+    rtc_time_t t;
+
+    rtc_get_time(&t);
+
+    len = snprintf(message, sizeof(message), "%d%02d", t.hours, t.mins);
+    fb_clear(fb);
+
+    if (len == 3)
+      ofs = 4;
+    else
+      ofs = 0;
+
+    disp_str(fb, ofs, 0, message, len);
+
+    fb_to_ws2811disp(fb);
+
+    task_delay(2000);
+
+    rtc_get_time(&t);
+    if (t.hours <= 4 || t.hours >= 22)
+      snprintf(message, sizeof(message), "GOOD NIGHT");
+    else
+      snprintf(message, sizeof(message), "HAPPY %s", get_dayname(t.dayno));
+
+    len = strlen(message);
+
+    for (i = (int)(w); i >= -8 * len; i--) {
+      fb_clear(fb);
+
+      disp_str(fb, i, 0, message, len);
+
+      fb_to_ws2811disp(fb);
+
+      task_delay(100);
+    }
+  }
+}
+#else
+static void body_rainbow(void)
+{
+  unsigned int i, j, o;
+
+  for (j = 0; j < 256; j++) {
+
+    FAST_LOG('w', "ws2811 start\n", 0, 0);
+
+    for (i = 0; i < PIXELS; i++) {
+      o = i + j;
+      enc_col(i, scale(wheel(o & 255), 32), WSBIT);
+    }
+
+    FAST_LOG('w', "ws2811 end\n", 0, 0);
+
+    ws2811_tx();
+    task_delay(50);
+  }
+}
+
+#if 1
+#define COLS 0x808080, 0xff0000 /* red + white */
+#else
+#define COLS 0xffd700, 0x808080, 0x008000 /* green, white and gold */
+#endif
+
+static void body_colseq(void)
+{
+  static unsigned int col[] = { COLS };
+  unsigned int i, j, o;
+
+  for (j = 0; j < PIXELS; j++) {
+    for (i = 0; i < PIXELS; i++) {
+      o = (j + i) % ARRSIZ(col);
+      enc_col(i, scale(col[o], 10), 1);
+    }
+
+    ws2811_tx();
+    task_delay(2000);
+  }
+}
+
 void task_led(void *arg)
 {
   irq_register("ws2811", irq_ws2811, 0, WSIRQ);
 
   for (;;) {
-    unsigned int i, j, o;
-
-    for (j = 0; j < 256; j++) {
-
-      FAST_LOG('w', "ws2811 start\n", 0, 0);
-
-      for (i = 0; i < PIXELS; i++) {
-        o = i + j;
-        enc_col(i, scale(wheel(o & 255), 10), WSBIT);
-      }
-
-      FAST_LOG('w', "ws2811 end\n", 0, 0);
-
-      ws2811_tx();
-      task_delay(50);
-    }
+    if (1)
+      body_rainbow();
+    else
+      body_colseq();
   }
-#if 0
-#if 1
-  /* red + white */
-  unsigned int col[] = { 0x808080, 0xff0000 };
-#else
-  /* green, white and gold */
-  unsigned int col[] = { 0xffd700, 0x808080, 0x008000 };
-#endif
-  for (;;) {
-    unsigned int i, j, o;
-
-    for (j = 0; j < PIXELS; j++) {
-      for (i = 0; i < PIXELS; i++) {
-        o = (j + i) % ARRSIZ(col);
-        enc_col(i, scale(col[o], 10), 1);
-      }
-
-      ws2811_tx();
-      task_delay(2000);
-    }
-  }
-#endif
 }
+#endif
