01295324982dccec534e5b1e1065b7164e2d3e7f
Brian Murphy <brm@robotize.com>
2024-08-16 16:18:14 +0200

    mshell header fixes

--- modules/appl/shell/inc/mshell.h
+++ modules/appl/shell/inc/mshell.h
@@ -1,10 +1,15 @@
 #ifndef MSHELL_H
 #define MSHELL_H
 
+#include "bmos_msg_queue.h"
+#include "hal_uart.h"
+
 void polled_shell(void);
 void mshell_add_uart(uart_t *u, unsigned int baud,
                      unsigned int num, unsigned int txop);
+void mshell_add_queue(bmos_queue_t *q, unsigned int num, unsigned int txop);
 void mshell_init(const char *name, unsigned int dest);
 int _xgetc(int timeout);
+bmos_queue_t *mshell_queue();
 
 #endif
